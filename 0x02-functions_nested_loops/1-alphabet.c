612ad2fcfa95e5c0de3f199cb5c5e57fd92a1cb6
MrCYK83 <nyam83@gmail.com>
2023-06-21 23:03:39 +0000

    C Functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,12 @@
 /**
  * main - prints alphabets in lowercase
  *
- * Return: Always 0
+ * Return: Always 0 on success
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
